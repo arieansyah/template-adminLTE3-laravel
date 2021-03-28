55087bcffdd3148fb54a03f5944204dd600455ed
arieansyah <arieansyahp.bun@gmail.com>
2021-03-28 12:10:14 +0700

    + example treeview

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,19 +4,44 @@
         <!-- Add icons to the links using the .nav-icon class
    with font-awesome or any other icon font library -->
         <li class="nav-item">
-            <a href="{{ route('admin/') }}" class="nav-link">
+            <a href="{{ route('admin/') }}" class="nav-link {{ setActive('admin/') }}">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>Dashboard</p>
             </a>
         </li>
 
         <li class="nav-item">
-            <a href="{{ route('admin/categories/index') }}" class="nav-link">
+            <a href="{{ route('admin/categories/index') }}"
+                class="nav-link {{ setActive('admin/categories/index') }}">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>Category</p>
             </a>
         </li>
-        
+
+        <li class="nav-item {{ setMenuOpen(['admin/tes1', 'admin/tes2']) }}">
+            <a href="#" class="nav-link {{ setActive(['admin/tes1', 'admin/tes2']) }}">
+                <i class="nav-icon fas fa-edit"></i>
+                <p>
+                    Treeview
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{ route('admin/tes1') }}" class="nav-link {{ setActive('admin/tes1') }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Tes1</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin/tes2') }}" class="nav-link {{ setActive('admin/tes2') }}">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Tes2</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
         <li class="nav-item">
             <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
             document.getElementById('logout-form').submit();">
