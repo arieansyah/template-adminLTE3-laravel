c2f75d5635e4638f641cb226ce649b1277058bb2
arieansyah <arieansyahp.bun@gmail.com>
2021-03-28 12:09:53 +0700

    debug page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,7 @@
 
                 <p>
                     We could not find the page you were looking for.
-                    Meanwhile, you may <a href="{{ route('/') }}">return to dashboard</a> or try using the search form.
+                    Meanwhile, you may <a href="{{ url('/') }}">return to dashboard</a> or try using the search form.
                 </p>
 
                 <form class="search-form">
